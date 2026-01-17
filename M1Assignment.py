d0de8c1451ecd761d0d327ddc5a6529f457775bb
Peter <pricci1974@gmail.com>
2026-01-17 20:20:09 +0000

    M1 Assignment

---
+++ M1Assignment.py
@@ -0,0 +1,6 @@
+my_name = "Peter"
+print("Hello, my name is " + my_name + ", and I am learning Python!")
+first_number = 10
+second_number = 5
+print(f"The sum of {first_number} and {second_number} is {first_number + second_number}.")
+
