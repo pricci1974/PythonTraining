101bb2902c81e5c96d9cc337771e0578db89da00
Peter <pricci1974@gmail.com>
2026-04-18 17:59:25 +0000

    Updated

--- conda/PythonFinal/nwsclient.py
+++ conda/PythonFinal/nwsclient.py
@@ -12,8 +12,16 @@ class NWSClient:
 
     def get_forecast(self):
         url = f"{self.base_url}"
-        response = requests.get(url, headers=self.headers)
-        response.raise_for_status()
-        data = response.json()
-        return data["properties"]["periods"]
-    
+        try:
+            response = requests.get(url, headers=self.headers)
+            if response.status_code == 200:
+                response.raise_for_status()
+                data = response.json()
+                return data["properties"]["periods"]
+            else:
+                print(f"Error: Received status code {response.status_code}")
+                quit("Error obtaining data")
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+            quit("Error - exiting program")
+            
